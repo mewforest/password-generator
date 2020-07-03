cbc73cefba57a606df4bceea48c306c362fa2697
Mew Forest <mew.forest@gmail.com>
2020-07-03 21:11:05 +0300

    Added range input to settings

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import IntegerField, SubmitField
+from wtforms.fields.html5 import IntegerRangeField
 from wtforms.validators import DataRequired, NumberRange
 
 
 class Settings(FlaskForm):
-    letters_number = IntegerField('Количество символов', default=6,
+    letters_number = IntegerRangeField('Количество символов', default=6,
                                   validators=[DataRequired(), NumberRange(min=1, max=32)])
-    passwords_number = IntegerField('Количество паролей', default=4,
+    passwords_number = IntegerRangeField('Количество паролей', default=4,
                                     validators=[DataRequired(), NumberRange(min=1, max=20)])
     submit = SubmitField('Применить настройки')
